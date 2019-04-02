c1de6fec783a819049bedf6a288a6bd01205375b
Raul <raul11jg@gmail.com>
2019-04-02 10:30:01 +0200

    Revert "base del juego y update"
    This reverts commit b6dc2346bb060158e7aec9b837aaa6bfb2b07b29.

--- raid-humans/src/juego.cpp
+++ raid-humans/src/juego.cpp
@@ -5,34 +5,31 @@ juego::juego(Vector2u resolucion)
     //ctor
 inicializar();
  ventana = new RenderWindow (VideoMode(resolucion.x, resolucion.y), "Raid humans");
- while(ventana->isOpen()){
-    update();
-    renderizar();
- }
 
 }
-/*
+
 juego::~juego()
 {
     //dtor
 
 
 }
-*/
-
-void juego:: inicializar() { //inicializar las variables del juego
 
 
-}
+juego::void inicializar() { //inicializar las variables del juego
 
-
-void juego:: update(){
 
 
 }
 
 
-void juego:: renderizar(){
-    ventana->display();
+juego::void update(){
+
+
+}
+
+
+juego::void renderizar(){
+
 
 }
