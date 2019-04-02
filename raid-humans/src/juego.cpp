b6dc2346bb060158e7aec9b837aaa6bfb2b07b29
Raul <raul11jg@gmail.com>
2019-04-02 10:30:01 +0200

    base del juego y update
    falte que update vaya al tiempo

--- raid-humans/src/juego.cpp
+++ raid-humans/src/juego.cpp
@@ -5,31 +5,34 @@ juego::juego(Vector2u resolucion)
     //ctor
 inicializar();
  ventana = new RenderWindow (VideoMode(resolucion.x, resolucion.y), "Raid humans");
+ while(ventana->isOpen()){
+    update();
+    renderizar();
+ }
 
 }
-
+/*
 juego::~juego()
 {
     //dtor
 
 
 }
+*/
 
-
-juego::void inicializar() { //inicializar las variables del juego
-
-
-
-}
-
-
-juego::void update(){
+void juego:: inicializar() { //inicializar las variables del juego
 
 
 }
 
 
-juego::void renderizar(){
+void juego:: update(){
 
+
+}
+
+
+void juego:: renderizar(){
+    ventana->display();
 
 }
